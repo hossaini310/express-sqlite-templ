4f7e7806fabdccf092ec098297506c9856c97bbf
Hossaini Hosain <hossainih03@gmail.com>
2023-04-15 01:58:40 +0200

    🔥 chore(db): remove unused db.sqlite3 file 🔨 refactor(movies.js): simplify dbGetMovie function The unused db.sqlite3 file has been removed. The dbGetMovie function has been simplified by removing the unnecessary result variable and returning the query directly.

--- src/api/models/movies.js
+++ src/api/models/movies.js
@@ -2,9 +2,6 @@ import query from '../../db/index.js';
 
 const dbGetMovies = () => query('SELECT * FROM movies');
 
-const dbGetMovie = (id) => {
-  const result = query('SELECT * FROM movies WHERE id = $1', [id]);
-  return result;
-};
+const dbGetMovie = (id) => query('SELECT * FROM movies WHERE id = $1', [id]);
 
 export { dbGetMovies, dbGetMovie };
